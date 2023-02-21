bbd353ba84033c17169ffabaa581da2ccc2352da
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-02-21 11:45:10 -0500

    Update Constants.java

--- src/game/Constants.java
+++ src/game/Constants.java
@@ -4,5 +4,17 @@ public final class Constants {
     private Constants() {
         // no need for constructor
     }
+    
+    
+    // controls the delay between each tick in ms
+    private final int DELAY = 25;
+    // controls the size of the board
+    public static final int TILE_SIZE = 50;
+    public static final int ROWS = 10;
+    public static final int COLUMNS = 10;
+    // controls how many apples appear on the board
+    public static final int NUM_APPLES = 5;
+    // suppress serialization warning
+    private static final long serialVersionUID = 490905409104883233L;
 
 }
